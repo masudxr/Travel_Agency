8ed3427569ed9d0aae27e1b1de890f1cc40d6a12
masudxr <masudxr@gmail.com>
2023-10-01 16:25:51 +0600

    first commit

---
+++ vite_practice/src/App.jsx
@@ -0,0 +1,12 @@
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <div className="bg-indigo-600 w-full h-screen"></div>
+    </>
+  );
+}
+
+export default App;
